0e528f306b4393dab38173cae15244e810be5f1c
duda <duda@6dc93482-27b8-4c24-8500-90c78a5336cd>
2013-04-05 21:02:57 +0000

    Remove superfluous argument "*id" to sprintf when directing standard error and standard out to /dev/null.
    M    src/framework/streams.c
    
    
    git-svn-id: https://svn-mpas-model.cgd.ucar.edu/trunk/mpas@2734 6dc93482-27b8-4c24-8500-90c78a5336cd

--- src/framework/streams.c
+++ src/framework/streams.c
@@ -34,14 +34,14 @@ void open_streams(int * id)
 		   return;
 	   }
    } else {
-	   sprintf(fname, "/dev/null", *id);
+	   sprintf(fname, "/dev/null");
 	   fd_err = open(fname,O_CREAT|O_WRONLY|O_TRUNC,0644);
 	   if (dup2(fd_err, 2) < 0) {
 		   printf("Error duplicating STDERR\n");
 		   return;
 	   }
 
-	   sprintf(fname, "/dev/null", *id);
+	   sprintf(fname, "/dev/null");
 	   fd_out = open(fname,O_CREAT|O_WRONLY|O_TRUNC,0644);
 	   if (dup2(fd_out, 1) < 0) {
 		   printf("Error duplicating STDOUT\n");
